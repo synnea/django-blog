088392b8d024ac3086b1a1cf6d8dcd29b5f330d0
Carina <carrie.poell@gmail.com>
2019-12-22 22:26:36 +0100

    finish adding templates.

--- posts/views.py
+++ posts/views.py
@@ -3,19 +3,26 @@ from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
 
-def get_posts(request):
-    """ Creates a list that returns a list of posts that were
-    published prior to now. """
 
-    posts = Post.object.filter(published_date__lte=timezone.now()).order_by('-published_date')
+def get_posts(request):
+    """
+    Create a view that will return a list
+    of Posts that were published prior to 'now'
+    and render them to the 'blogposts.html' template
+    """
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+        ).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
 
 
 def post_detail(request, pk):
-    """ Create a view that returns a single post object
-    based on the ID (pk) and render it to the post detail.html
-    template. Or return error if not found. """
-
+    """
+    Create a view that returns a single
+    Post object based on the post ID (pk) and
+    render it to the 'postdetail.html' template.
+    Or return a 404 error if the post is
+    not found
+    """
     post = get_object_or_404(Post, pk=pk)
     post.views += 1
     post.save()
@@ -23,9 +30,11 @@ def post_detail(request, pk):
 
 
 def create_or_edit_post(request, pk=None):
-    """ Create a view that allows us to create or edit a post,
-    depending on if the post id is null or not """
-
+    """
+    Create a view that allows us to create
+    or edit a post depending if the Post ID
+    is null or not
+    """
     post = get_object_or_404(Post, pk=pk) if pk else None
     if request.method == "POST":
         form = BlogPostForm(request.POST, request.FILES, instance=post)
@@ -35,7 +44,3 @@ def create_or_edit_post(request, pk=None):
     else:
         form = BlogPostForm(instance=post)
     return render(request, 'blogpostform.html', {'form': form})
-
-
-
-
